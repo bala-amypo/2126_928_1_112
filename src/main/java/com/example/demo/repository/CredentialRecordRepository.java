f7f3441006eaad4cd3c32c1f6e982e7529c11eba
bala-amypo <bala@amypo.in>
2025-12-19 08:36:00 +0000

    User commit

--- src/main/java/com/example/demo/repository/CredentialRecordRepository.java
+++ src/main/java/com/example/demo/repository/CredentialRecordRepository.java
@@ -1,7 +1,6 @@
-package com.example.demo.repository;
-
-import java.time.LocalDate;
+import java.util.Optional;
 import java.util.List;
+import java.time.LocalDate;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -11,5 +10,8 @@ public interface CredentialRecordRepository
         extends JpaRepository<CredentialRecord, Long> {
 
     List<CredentialRecord> findByExpiryDateBefore(LocalDate date);
+
+    Optional<CredentialRecord> findByCredentialCode(String credentialCode);
+
+    List<CredentialRecord> findByHolderId(Long holderId);
 }
-    
