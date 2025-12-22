f251d6c7b22fbfe78795bac65e6524ce2d714b28
bala-amypo <bala@amypo.in>
2025-12-22 10:16:41 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -0,0 +1,44 @@
+package com.example.demo.service;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+
+@Service
+public class UserServiceImpl implements AuthService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User register(User user) {
+
+        userRepository.findByEmail(user.getEmail()).ifPresent(u -> {
+            throw new RuntimeException("Email already exists");
+        });
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRole("VIEWER");
+
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User login(String email, String password) {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("Invalid email"));
+
+        if (!passwordEncoder.matches(password, user.getPassword())) {
+            throw new RuntimeException("Invalid password");
+        }
+
+        return user;
+    }
+}
